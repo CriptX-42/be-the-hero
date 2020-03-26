6c32671057c6efcdb62b48bf6b386ed832cdd275
ricardo <ricardo-svc@live.com>
2020-03-25 23:11:22 -0300

    Iniciando o backend

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({
+    evento: "Semana omnistack 11.0",
+    aluno: "Ricardo Carvalho"
+  });
+});
