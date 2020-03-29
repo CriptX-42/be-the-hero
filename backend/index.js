c0c1255fa5bcdbd7057d08b8e183cbc870c2abf6
ricardo <ricardo-svc@live.com>
2020-03-28 23:51:01 -0300

    pegando os parametros da requisição

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 
 app.get("/users", (request, response) => {
+  const params = request.query;
+  console.log(params);
   return response.json({
     evento: "Semana omnistack 11.0",
     aluno: "Ricardo Carvalho"
