3f3d26e7f0521783c15df2cdf6150a39b6e8f289
ricardo <ricardo-svc@live.com>
2020-03-29 17:26:16 -0300

    Fazendo post de incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngsController');
-
-const connection = require('./database/connection');
+const Incidentontroller = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
+//* Ongs
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
+
+//* Incidents
+routes.post('/incidents', Incidentontroller.create);
 
 module.exports = routes;
