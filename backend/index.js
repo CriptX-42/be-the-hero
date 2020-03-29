17e7f77893d98375f96495f70fb600b458a9ffc6
ricardo <ricardo-svc@live.com>
2020-03-28 23:55:19 -0300

    Comentado os tipos de parametrôs

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+/* *
+ * Tipos de parametros
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginações)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Ciroi da requisição, utilizado para criar ou alterar outros recursos
+ */
+
 app.get("/users/:id", (request, response) => {
   const params = request.params;
   console.log(params);
