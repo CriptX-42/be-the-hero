d5c3c69a199ce88cb43a1a03cf2335da87ed616d
ricardo <ricardo-svc@live.com>
2020-03-29 18:09:12 -0300

    Criando parte de perfil

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngsController');
-const Incidentontroller = require('./controllers/IncidentController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
@@ -9,9 +10,12 @@ const routes = express.Router();
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+//* Profile
+routes.get('/profile', ProfileController.index);
+
 //* Incidents
-routes.get('/incidents', Incidentontroller.index);
-routes.post('/incidents', Incidentontroller.create);
-routes.delete('/incidents/:id', Incidentontroller.delete);
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
