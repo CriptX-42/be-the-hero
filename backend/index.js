f27415e5de654bf6a383a48763aaf6623150a1fc
ricardo <ricardo-svc@live.com>
2020-03-28 23:48:11 -0300

    voltando a user

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/users:id", (request, response) => {
+app.get("/users", (request, response) => {
   return response.json({
     evento: "Semana omnistack 11.0",
     aluno: "Ricardo Carvalho"
