ebe369c9c582af08353ad400e7fc6a65c3a8e03b
ricardo <ricardo-svc@live.com>
2020-03-29 19:42:52 -0300

    Terminando a parte do backend e instalando o cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,11 @@
-const express = require("express");
-const routes = require("./routes");
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-/* *
- * Tipos de parametros
- * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginações)
- * Route Params: Parâmetros utilizados para identificar recursos
- * Request Body: Ciroi da requisição, utilizado para criar ou alterar outros recursos
- */
-
 app.use(routes);
 
 app.listen(3333);
