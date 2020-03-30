82afadf99a0576a5b7d4d219974cffc36d6523b5
ricardo <ricardo-svc@live.com>
2020-03-29 22:32:19 -0300

    Fazendo todo o estilo da pagina de logon

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { FiLogIn } from "react-icons/fi";
+
 import "./style.css";
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
 
 export default function logon() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="div logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID" type="text" />
+          <button className="button" type="submit">
+            Entrar
+          </button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="e02141" />
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
 }
