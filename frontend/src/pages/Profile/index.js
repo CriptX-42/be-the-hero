0e2685643bb3404027d3184a4ac4385f79797415
ricardo <ricardo-svc@live.com>
2020-03-30 20:41:32 -0300

    Ajustando aplicação com prettier

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FiPower, FiTrash2 } from "react-icons/fi";
-import "./style.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import './style.css';
 
-import logoImg from "../../assets/logo.svg";
+import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
   return (
